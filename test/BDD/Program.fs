e11e089d6760346d9879411a0e9cee70994340b9
Onur Gumus <onurgumus@microsoft.com>
2022-04-12 13:30:58 +0200

    add test

--- test/BDD/Program.fs
+++ test/BDD/Program.fs
@@ -13,6 +13,7 @@ open Akkling.Streams.TestKit.Probes
 open Akkling.Streams.TestKit.ManualPublisherProbe
 open Akkling.Streams.TestKit.ManualSubscriberProbe
 open Akka.TestKit
+open System.Threading
 
 let config () = Configuration.parse "akka.logLevel = DEBUG"
 
@@ -21,20 +22,34 @@ let ``Graph DSL operators should work`` () = test (config()) <| fun tck ->
     let probe = manualSubscriberProbe tck
     let oneSec = TimeSpan.FromSeconds 1
     Source.tick oneSec oneSec 1
+    |> Source.take 2
     |> Source.runWith mat (Sink.ofSubscriber probe)
 
     let sub = expectSubscription probe
     sub.Request 1000L
     let sch = tck.Sys.Scheduler :?> TestScheduler
+
+    
+    let sleep t = Async.FromContinuations(fun (cont, erFun, _) ->
+        sch.ScheduleOnce(t, cont)
+    )
+    async{
+       do! sleep (TimeSpan.FromMilliseconds 2100)
+       printf "%A" sch.Now
+    }|> Async.Start
+
+    Thread.Sleep(100)
+    sch.Advance oneSec
     sch.Advance oneSec
     sch.Advance oneSec
     probe
     |> expectNext 1
     |> expectNext 1
-  //  |> expectComplete
+    |> expectComplete
     |> ignore
 
 [<EntryPoint>]
 let main v =
     ``Graph DSL operators should work``()
+    Console.ReadKey() |> ignore
     0
